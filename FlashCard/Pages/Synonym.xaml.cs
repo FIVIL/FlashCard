9dd783d6dcb89dc50223079d403f3bb0295ee06b
hamed Mohammedi <hammedmohamadi@gmail.com>
2019-04-26 15:13:06 +0430

    loading

--- FlashCard/Pages/Synonym.xaml.cs
+++ FlashCard/Pages/Synonym.xaml.cs
@@ -20,26 +20,24 @@ namespace FlashCard.Pages
     /// <summary>
     /// Interaction logic for Synonym.xaml
     /// </summary>
-    public partial class Synonym : UserControl, IDisposable
+    public partial class Synonym : UserControl
     {
         private List<Word> Words;
         private Word Current => Words[index];
         private int index;
         private Dictionary dic;
         private SpeechSynthesizer syn;
+        private Random rnd;
         public Synonym()
         {
             InitializeComponent();
-            index = -1;
-            dic = new Dictionary();
-            Words = dic.GetAll().ToList();
-            var rnd = new Random();
-            Words = Words.OrderBy(x => rnd.Next(Words.Count)).ToList();
+            Helpers.mainWindow.Height = 350;
             syn = new SpeechSynthesizer();
+            dic = new Dictionary();
+            rnd = new Random();
             syn.Volume = 100;
             syn.Rate = -2;
-            next();
-            Helpers.mainWindow.Height = 350;
+            Load()
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -77,17 +75,53 @@ namespace FlashCard.Pages
             else MessageBox.Show("ended");
         }
 
-        public void Dispose()
-        {
-            ((IDisposable)dic).Dispose();
-            syn.Dispose();
-        }
-
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
             Definition.Text = Current.Definitions;
             Persian.Text = Current.Persian;
             Pron.Text = Current.Pron;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            var ci = CountImportance.Text;
+            if (string.IsNullOrWhiteSpace(ci))
+            {
+                Load();
+            }
+            else if (!ci.Contains(':'))
+            {
+                Load(int.Parse(ci));
+            }
+            else
+            {
+                var p = ci.Split(':');
+                if (string.IsNullOrWhiteSpace(p[0])) Load(0, int.Parse(p[1]));
+                else Load(int.Parse(p[0]), int.Parse(p[1]));
+            }
+
+        }
+        private void Load()
+        {
+            index = -1;
+            Words = dic.GetAll().ToList();
+            Words = Words.OrderBy(x => rnd.Next(Words.Count)).ToList();
+            next();
+        }
+        private void Load(int max)
+        {
+            index = -1;
+            Words = dic.GetAll().ToList();
+            Words = Words.OrderBy(x => rnd.Next(Words.Count)).Take(max).ToList();
+            next();
+        }
+        private void Load(int max, int diff)
+        {
+            index = -1;
+            Words = dic.GetWord(x => x.Meaning < diff).ToList();
+            if (max == 0) Words = Words.OrderBy(x => rnd.Next(Words.Count)).ToList();
+            else Words = Words.OrderBy(x => rnd.Next(Words.Count)).Take(max).ToList();
+            next();
         }
     }
 }
